f2b09fef0f99f6cca81e6e8b44bf034e15e208d0
Luis Alejandro <128195940+TWRebot@users.noreply.github.com>
2024-08-05 22:53:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const meuNome = "luis";
+const idade = 58;
+const altura = 1.85;
+const massa = 75;
+const cidade = "São josé dos pinhais";
+console.log(meuNome);
+console.log(idade);
+console.log(altura);
+console.log(massa);
+console.log(cidade);
